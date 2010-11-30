deec82d08a1e3bf735b2b87c2e00677009bb1d17
Jakub Korzeniowski <jakub.korzeniowski@gmail.com>
2010-11-30 17:30:22 +0100

    Version 0.4\n A few operators added.

--- airContainer.h
+++ airContainer.h
@@ -5,6 +5,7 @@ using namespace std;
 template <typename T>
 class airContainer
 {
+	public:
 	void append(T const & element);
 	bool replace(int index, T const & element);
 	void clear(void);
@@ -25,8 +26,17 @@ class airContainer
 	airContainer<T> & operator+=(T const & element);
 	airContainer<T> & operator<<(airContainer<T> const & right);
 	airContainer<T> & operator<<(T const & element);
+
+	protected:
+	int checkEach(T const & element, bool invert, int from = 0);
+	bool compare(airContainer<T> const & right);
 };
 
+// -----------------------------------------------------------------
+
+/**
+* \Definicje metod
+*/
 
 template <typename T> void airContainer<T>::append(T const & element)
 {
@@ -45,21 +55,67 @@ template <typename T> void airContainer<T>::clear(void)
 
 template <typename T> bool airContainer<T>::isEmpty(void)
 {
-	if(checkEach(NULL, true, this) >= 0)
+	if(this->checkEach(NULL, true) >= 0)
 		return true;
 	return false;
 }
 
 template <typename T> int airContainer<T>::indexOf(T const & _value, int _from) const
 {
-	return checkEach(_value, false, this, _from);
+	return this->checkEach(_value, false, _from);
 }
 
 template <typename T> bool airContainer<T>::contains(T const & _value, int _from) const
 {
-	if(checkEach(_value, false, this, _from) >= 0)
+	if(this->checkEach(_value, false, _from) >= 0)
 		return true;
 	return false; 
+}
+
+// -----------------------------------------------------------------
+
+/**
+* \Definicje operatorow
+*/
+
+template <typename T> airContainer<T> & airContainer<T>::operator=(airContainer<T> const & right)
+{
+
+}
+
+template <typename T> bool airContainer<T>::operator==(airContainer<T> const & right)
+{
+	return this->compare(right);
+}
+
+template <typename T> bool airContainer<T>::operator!=(airContainer<T> const & right)
+{
+	return !(this->compare(right));
+}
+
+template <typename T> T & airContainer<T>::operator[](int n) const
+{
+	return this->at(n);
+}
+
+template <typename T> airContainer<T> & airContainer<T>::operator+=(airContainer<T> const & right)
+{
+
+}
+
+template <typename T> airContainer<T> & airContainer<T>::operator+=(T const & element)
+{
+
+}
+
+template <typename T> airContainer<T> & airContainer<T>::operator<<(airContainer<T> const & right)
+{
+
+}
+
+template <typename T> airContainer<T> & airContainer<T>::operator<<(T const & element)
+{
+
 }
 
 
@@ -73,7 +129,11 @@ template <typename T> bool airContainer<T>::contains(T const & _value, int _from
 template <typename T>
 ostream & operator<<(ostream & output, airContainer<T> const & right)
 {
-
+	int size = right.size();
+	
+	for(int i = 0; i < size; i++)
+		output << "element " << i << ": " << right.at(i) << endl;
+	return output << endl;
 }
 
 
@@ -88,17 +148,33 @@ ostream & operator<<(ostream & output, airContainer<T> const & right)
 ///
 /// \param element - element, z ktorym porownywanie sa elementy pojemnika
 /// \param invert - jezeli true, to odwraca warunek ("==" -> "!=")
-/// \param container - referencja do pojemnika
 /// \param from - element, od ktorego zaczynamy iteracje 
 /// \return index elementu, jezeli natrafi na element w pojemniku, -1, jezeli nie natrafi
-template <typename T> int checkEach(T const & element, bool invert, airContainer<T> const & container, int from = 0)
+template <typename T> int airContainer<T>::checkEach(T const & element, bool invert, int from)
 {
-	int size = container.size();
+	int size = this->size();
 
 	from = from < 0 ? 0 : from;	
 
 	for(int i = from; i < size; i++)
-		if((container.at(i) == element) - invert)
+		if((this->at(i) == element) - invert)
 			return i;
 	return -1;		
 } 
+
+/// \compare - porownuje zawartosci dwoch pojemnikow
+///
+/// \param right - referencja do pojemnika, z ktorym ma byc porownany obiekt
+/// \return true jezeli pojemniki sa rowne, false, jezeli sa rozne
+template <typename T> bool airContainer<T>::compare(airContainer<T> const & right)
+{
+	int size = this->size(), rightSize = right.size();
+	if(size == rightSize)
+	{
+		for(int i = 0; i < size; i++)
+			if(this->at(i) != right.at(i))
+				return false;
+		return true;
+	}
+	return false;
+}
