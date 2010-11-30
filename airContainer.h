d0a994c45ce748a4ddb287503de7141d171da710
Jakub Korzeniowski <jakub.korzeniowski@gmail.com>
2010-11-30 16:20:18 +0100

    Version 0.3\n isEmpty, indexOf and contains methods added.

--- airContainer.h
+++ airContainer.h
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-template<typename T>
+template <typename T>
 class airContainer
 {
 	void append(T const & element);
@@ -27,5 +27,78 @@ class airContainer
 	airContainer<T> & operator<<(T const & element);
 };
 
-template<typename T>
-ostream & operator<<(ostream & output, airContainer<T> const & right);
+
+template <typename T> void airContainer<T>::append(T const & element)
+{
+
+}
+
+template <typename T> bool airContainer<T>::replace(int index, T const & element)
+{
+
+}
+
+template <typename T> void airContainer<T>::clear(void)
+{
+
+}
+
+template <typename T> bool airContainer<T>::isEmpty(void)
+{
+	if(checkEach(NULL, true, this) >= 0)
+		return true;
+	return false;
+}
+
+template <typename T> int airContainer<T>::indexOf(T const & _value, int _from) const
+{
+	return checkEach(_value, false, this, _from);
+}
+
+template <typename T> bool airContainer<T>::contains(T const & _value, int _from) const
+{
+	if(checkEach(_value, false, this, _from) >= 0)
+		return true;
+	return false; 
+}
+
+
+// -----------------------------------------------------------------
+
+/// \operator << dla klasy airContainer
+///
+/// \param output - strumien wyjscia
+/// \param right - referencja do obiektu klasy airContainer, ktorej elementy maja byc wyswietlone
+/// \return strumien wyjscia
+template <typename T>
+ostream & operator<<(ostream & output, airContainer<T> const & right)
+{
+
+}
+
+
+// -----------------------------------------------------------------
+
+/**
+* \Helper functions
+* \martwia sie o powtarzajace sie kawalki kodu
+*/
+
+/// \checkEach - iteruje przez caly pojemnik i jezeli natrafi na element podany jako parametr "element" zwraca jego index
+///
+/// \param element - element, z ktorym porownywanie sa elementy pojemnika
+/// \param invert - jezeli true, to odwraca warunek ("==" -> "!=")
+/// \param container - referencja do pojemnika
+/// \param from - element, od ktorego zaczynamy iteracje 
+/// \return index elementu, jezeli natrafi na element w pojemniku, -1, jezeli nie natrafi
+template <typename T> int checkEach(T const & element, bool invert, airContainer<T> const & container, int from = 0)
+{
+	int size = container.size();
+
+	from = from < 0 ? 0 : from;	
+
+	for(int i = from; i < size; i++)
+		if((container.at(i) == element) - invert)
+			return i;
+	return -1;		
+} 
