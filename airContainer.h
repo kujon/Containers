da531d0548985b788e7f4c921cb34b70da499b15
Jakub Korzeniowski <jakub.korzeniowski@gmail.com>
2010-11-30 23:53:51 +0100

    Version 0.5\n All non-virtual methods, all operators defined (except =), doxygen comments added.

--- airContainer.h
+++ airContainer.h
@@ -30,6 +30,7 @@ class airContainer
 	protected:
 	int checkEach(T const & element, bool invert, int from = 0);
 	bool compare(airContainer<T> const & right);
+	airContainer<T> & appendContainer(airContainer<T> const & right);
 };
 
 // -----------------------------------------------------------------
@@ -38,21 +39,45 @@ class airContainer
 * \Definicje metod
 */
 
+/// \append - dodaje podany jako parametr obiekt typu T do pojemnika
+///
+/// \param element - element, ktory ma zostac dodany
 template <typename T> void airContainer<T>::append(T const & element)
 {
-
+	int size = this->size();
+	
+	this->insert(size, element);
 }
 
+/// \remove - podmienia obiekt typu T z obiektem na wybranym miejscu. 
+///
+/// \param index - miejsce, w ktorym dokonujemy podmiany
+/// \param element - element, na ktory podmieniamy
+/// \return Zwraca true jezeli podane miejsce usuniecia jest prawidlowe, w przeciwnym wypdaku zwraca false.
 template <typename T> bool airContainer<T>::replace(int index, T const & element)
 {
+	int size = this->size();
 
+	if(index >= 0 && index < size)
+	{
+		this->at(index) = element;
+		return true;
+	}
+	return false;
 }
 
+/// \clear - usuwa wszystkie elementy pojemnika
 template <typename T> void airContainer<T>::clear(void)
 {
+	int size = this->size();
 
+	for(int i = 0; i < size; i++)
+		this->remove(i);
 }
 
+/// \isEmpty - sprawdza czy pojemnik jest pusty
+///
+/// \return zwraca true jezeli pojemnik jest pusty, inaczej false   
 template <typename T> bool airContainer<T>::isEmpty(void)
 {
 	if(this->checkEach(NULL, true) >= 0)
@@ -60,11 +85,21 @@ template <typename T> bool airContainer<T>::isEmpty(void)
 	return false;
 }
 
+/// \indexOf - wyszukuje i zwraca indeks pierwszego elementu o wartosci _value zaczynajac od elementu _from
+///
+/// \param _value - wartosc, ktora ma zostac wyszukana
+/// \param _from - index, od ktorego zaczynamy przeszukiwanie
+/// \return index elementu, jezeli go nie ma, zwraca -1
 template <typename T> int airContainer<T>::indexOf(T const & _value, int _from) const
 {
 	return this->checkEach(_value, false, _from);
 }
 
+/// \contains - sprawdza czy dany elelment jest juz w pojemniku
+///
+/// \param _value - wartosc, ktora sprawdzamy
+/// \param _from - index, od ktorego zaczynamy przeszukiwanie
+/// \return zwraca true jezeli element o wartosci _value znajduje sie w pojemniku inaczej zwraca false
 template <typename T> bool airContainer<T>::contains(T const & _value, int _from) const
 {
 	if(this->checkEach(_value, false, _from) >= 0)
@@ -78,44 +113,78 @@ template <typename T> bool airContainer<T>::contains(T const & _value, int _from
 * \Definicje operatorow
 */
 
+/// \ = operator przypisania
+///
+/// \param right - pojemnik, ktory ma zostac przypisany
+/// \return referencja do pojemnika, do ktorego przypisalismy
 template <typename T> airContainer<T> & airContainer<T>::operator=(airContainer<T> const & right)
 {
 
 }
 
+/// \ == sprawdza, czy dwa pojemniki sa sobie rowne
+///
+/// \param right - pojemnik, z ktorym porownujemy
+/// \return true jezeli oba pojemniki sa sobie rowne, false w przeciwnym wypadku
 template <typename T> bool airContainer<T>::operator==(airContainer<T> const & right)
 {
 	return this->compare(right);
 }
 
+/// \ != sprawdza, czy dwa pojemniki sa rozne
+///
+/// \param right - pojemnik, z ktorym porownujemy
+/// \return true jezeli pojemniki sa rozne, false w przeciwnym wypadku
 template <typename T> bool airContainer<T>::operator!=(airContainer<T> const & right)
 {
 	return !(this->compare(right));
 }
 
+/// \ [] zwraca referencje do n-tego elementu
+///
+/// \param n - index elementu, ktory zwracamy
+/// \return referencja do n-tego elementu
 template <typename T> T & airContainer<T>::operator[](int n) const
 {
 	return this->at(n);
 }
 
+/// \ += dodaje wszystkie elementy z pojemnika right 
+///
+/// \param right - pojemnik, ktorego elementy maja byc dodane do this
+/// \return referencja do this
 template <typename T> airContainer<T> & airContainer<T>::operator+=(airContainer<T> const & right)
 {
-
+	return appendContainer(right);
 }
 
+/// \ += dodaje element do pojemnika
+///
+/// \param element - element, ktory ma zostac dodany
+/// \return referencja do this
 template <typename T> airContainer<T> & airContainer<T>::operator+=(T const & element)
 {
-
+	this->append(element);
+	return this;
 }
 
+/// \ += dodaje wszystkie elementy z pojemnika right 
+///
+/// \param right - pojemnik, ktorego elementy maja byc dodane do this
+/// \return referencja do this
 template <typename T> airContainer<T> & airContainer<T>::operator<<(airContainer<T> const & right)
 {
-
+	return appendContainer(right);
 }
 
+/// \ += dodaje element do pojemnika
+///
+/// \param element - element, ktory ma zostac dodany
+/// \return referencja do this
 template <typename T> airContainer<T> & airContainer<T>::operator<<(T const & element)
-{
-
+{	
+	this->append(element);
+	return this;
 }
 
 
@@ -169,6 +238,7 @@ template <typename T> int airContainer<T>::checkEach(T const & element, bool inv
 template <typename T> bool airContainer<T>::compare(airContainer<T> const & right)
 {
 	int size = this->size(), rightSize = right.size();
+
 	if(size == rightSize)
 	{
 		for(int i = 0; i < size; i++)
@@ -177,4 +247,19 @@ template <typename T> bool airContainer<T>::compare(airContainer<T> const & righ
 		return true;
 	}
 	return false;
+}
+
+/// \appendContainer - dodaje na koniec pojemnika wszystkie elementy z pojemnika right
+///
+/// \param right - referencja do pojemnika, ktorego elementy trzeba dodac
+template <typename T> airContainer<T> & airContainer<T>::appendContainer(airContainer<T> const & right)
+{
+	int size = right.size();
+	
+	for(int i = 0; i < size; i++)
+	{
+		this->append(right.at(i));
+	}
+	
+	return this;
 }
